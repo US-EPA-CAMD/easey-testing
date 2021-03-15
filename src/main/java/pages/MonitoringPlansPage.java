27ad1bc0f68753910b650922d15fce315d3eb960
ACPR <yefimabramson@cvpcorp.com>
2021-03-15 19:43:05 -0400

    added header and footer test cases

--- src/main/java/pages/MonitoringPlansPage.java
+++ src/main/java/pages/MonitoringPlansPage.java
@@ -34,4 +34,52 @@ public class MonitoringPlansPage extends PageBase {
 
     @FindBy(css = "h1.title")
     public WebElement title;
+
+    @FindBy(css = "button.usa-button.menuBTN")
+    public WebElement menuBtn;
+
+    @FindBy(css = "div.mainMenu > nav")
+    public WebElement menuNav;
+
+    @FindBy(css = "button[aria-controls='Environmental TopicsMenuDropDown']")
+    public WebElement environmentalTopicsMenuDropDown;
+
+    @FindBy(css = "a[title='Environmental Topics']")
+    public List<WebElement> environmentalTopics;
+
+    @FindBy(css = "button[aria-controls='Laws and RegulationsMenuDropDown']")
+    public WebElement lawsAndRegulationsMenuDropDown;
+
+    @FindBy(css = "a[title='Laws and Regulations']")
+    public List<WebElement> lawsAndRegulations;
+
+    @FindBy(css = "button[aria-controls='About EPAMenuDropDown']")
+    public WebElement aboutEPAMenuDropDown;
+
+    @FindBy(css = "a[title='About EPA']")
+    public List<WebElement> aboutEPA;
+
+    @FindBy(id = "search-field")
+    public WebElement search;
+
+    @FindBy(id = "input-button-search")
+    public WebElement searchButton;
+
+    @FindBy(id = "collapsable")
+    public WebElement footerMenuBTN;
+
+    @FindBy(id = "menuContent")
+    public WebElement footerMenuContent;
+
+    @FindBy(id = "AccessiblityLink")
+    public WebElement footerAccessibility;
+
+    @FindBy(id = "PrivacyLink")
+    public WebElement footerPrivacy;
+
+    @FindBy(id = "PrivacyandSecurityNoticeLink")
+    public WebElement footerNotice;
+
+    @FindBy(id = "close")
+    public WebElement footerClose;
 }
---
+++ src/test/java/tests/UITests/UI_TC92_812_EPA_Search_in_Header.java
@@ -0,0 +1,42 @@
+package tests.UITests;
+
+import org.testng.annotations.Test;
+import pages.MonitoringPlansPage;
+import pages.SearchResultPage;
+import tests.utils.UITestBase;
+
+import java.util.ArrayList;
+
+public class UI_TC92_812_EPA_Search_in_Header extends UITestBase {
+
+    @Test
+    public void test() {
+//        Navigate to EASEY In
+//        https://easey-dev.app.cloud.gov/monitoring-plans
+        goTo("https://easey-dev.app.cloud.gov/monitoring-plans");
+
+        MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+        SearchResultPage searchResultPage = new SearchResultPage(driver);
+
+        verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+
+
+//        Click on the menu option that is available on the header.
+        click(monitoringPlansPage.menuBtn);
+//        Menu opens
+        verifyTrue(monitoringPlansPage.menuNav.isDisplayed());
+
+//        Confirm that the search box is available at the top of the menu option and is in accordance with the wireframes and acceptance criteria
+//
+//        Search box is visible
+        verifyTrue(monitoringPlansPage.search.isDisplayed());
+
+//        Enter something into the search box field and click on the magnifying glass to initiate the search.
+        input(monitoringPlansPage.search, "something");
+        click(monitoringPlansPage.searchButton);
+//                Then the system opens the link on the EPA website in a new tab to view the results
+        changeTab();
+        verifyTrue(searchResultPage.searchResults.getText().contains("something"));
+
+    }
+}
