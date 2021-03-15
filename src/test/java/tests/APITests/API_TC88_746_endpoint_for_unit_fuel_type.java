ca0933a288c288893dc3ef5204b02c065c284d5c
ACPR <yefimabramson@cvpcorp.com>
2021-03-15 17:25:03 -0400

    added endpoint tests

--- src/test/java/tests/APITests/API_TC88_746_endpoint_for_unit_fuel_type.java
+++ src/test/java/tests/APITests/API_TC88_746_endpoint_for_unit_fuel_type.java
@@ -1,4 +1,40 @@
 package tests.APITests;
 
-public class API_TC88_746_endpoint_for_unit_fuel_type {
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import tests.utils.APITestBase;
+
+public class API_TC88_746_endpoint_for_unit_fuel_type extends APITestBase {
+
+    @BeforeMethod
+    public void beforeMethod() {
+        super.beforeMethod();
+        super.setup("https://easey-dev.app.cloud.gov");
+    }
+
+    @Test
+    public void test() {
+        String url = "api/master-data-mgmt/fuel-types";
+
+//        Perform a GET request for the unit fuel type endpoint
+//        https://easey-dev.app.cloud.gov/api/master-data-mgmt/fuel-types
+        JSONArray res = getJSONArray(url);
+//        API returns results containing all valid unit fuel types
+//        And for each record will display:
+//
+//        Fuel Type Code
+//        Fuel Group Code
+//        Fuel Type Description
+        for (Object r : res) {
+            if (r instanceof JSONObject) {
+                JSONObject response = (JSONObject) r;
+                verifyTrue(response.has("fuelTypeCode"));
+                verifyTrue(response.has("fuelGroupCode"));
+                verifyTrue(response.has("fuelTypeDescription"));
+            } else
+                verifyFail("Response returned non JSONObject\n" + r.toString());
+        }
+    }
 }
--- src/test/java/tests/APITests/API_TC89_747_endpoint_for_control_technologies.java
+++ src/test/java/tests/APITests/API_TC89_747_endpoint_for_control_technologies.java
@@ -1,4 +1,40 @@
 package tests.APITests;
 
-public class API_TC89_747_endpoint_for_control_technologies {
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import tests.utils.APITestBase;
+
+public class API_TC89_747_endpoint_for_control_technologies extends APITestBase {
+
+    @BeforeMethod
+    public void beforeMethod() {
+        super.beforeMethod();
+        super.setup("https://easey-dev.app.cloud.gov");
+    }
+
+    @Test
+    public void test() {
+        String url = "api/master-data-mgmt/control-technologies";
+
+//        Perform a GET request for the control technologies endpoint
+//        https://easey-dev.app.cloud.gov/api/master-data-mgmt/control-technologies
+        JSONArray res = getJSONArray(url);
+//        API returns results containing all valid control technologies
+//        And for each record will display:
+
+//        Control Code
+//        Control Description
+//        Control Equip Param Code
+        for (Object r : res) {
+            if (r instanceof JSONObject) {
+                JSONObject response = (JSONObject) r;
+                verifyTrue(response.has("controlCode"));
+                verifyTrue(response.has("controlDescription"));
+                verifyTrue(response.has("controlEquipParamCode"));
+            } else
+                verifyFail("Response returned non JSONObject\n" + r.toString());
+        }
+    }
 }
