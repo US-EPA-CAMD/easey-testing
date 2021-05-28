207a3d3763564c743aca96dc5a80f78cf735b7f3
mosesdee <mosesdee@cvpcorp.com>
2021-05-28 15:29:47 -0400

    Added steps to 1414 along with updating the locators

--- src/test/java/tests/UITests/Test_EASEYIn_1414_Display_login_welcome.java
+++ src/test/java/tests/UITests/Test_EASEYIn_1414_Display_login_welcome.java
@@ -1,46 +1,63 @@
 package tests.UITests;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import org.testng.annotations.Test;
 import pages.MonitoringPlansPage;
 import tests.utils.UITestBase;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Test_EASEYIn_1414_Display_login_welcome extends UITestBase {
 
+    private static final String csvPath = "";
+
     @Test
-    public void tests () {
+    public void tests() throws IOException, CsvValidationException {
 
-        //Navigate to EASEY In
-        //https://easey-dev.app.cloud.gov/monitoring-plans
+        CSVReader reader = new CSVReader(new FileReader(csvPath));
+        String[] csvCell;
+        while ((csvCell = reader.readNext()) != null) {
+            String username = csvCell[0];
+            String password = csvCell[1];
 
-        goTo("https://easey-dev.app.cloud.gov/monitoring-plans");
+            //Navigate to EASEY In
+            //https://easey-dev.app.cloud.gov/monitoring-plans
 
-        MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+            goTo("https://easey-dev.app.cloud.gov/monitoring-plans");
 
-        verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+            MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
 
-        // Click Log In button on dashboard
-        verifyEquals(monitoringPlansPage.logInButton, "Log In");
-        click(monitoringPlansPage.logInButton);
-        // Log In modal pops up.
-        verifyEquals(monitoringPlansPage.logInLabel, "Log In");
-        // Enter Username
-        verifyEquals(monitoringPlansPage.usernameLabel, "Username");
-        monitoringPlansPage.usernameField.sendKeys("username");
-        // Enter Password
-        verifyEquals(monitoringPlansPage.passwordLabel, "Username");
-        monitoringPlansPage.passwordField.sendKeys("password");
-        // Click Log In
-        click(monitoringPlansPage.logInButton);
-        // Welcome note appears on Dashboard Navigation
+            verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+
+            // Click Log In button on dashboard
+            verifyEquals(monitoringPlansPage.logInButton, "Log In");
+            click(monitoringPlansPage.logInButton);
+            // Log In modal pops up.
+            verifyEquals(monitoringPlansPage.logInLabel, "Log In");
+            // Enter Username
+            verifyEquals(monitoringPlansPage.usernameLabel, "Username");
+            monitoringPlansPage.usernameField.sendKeys(username);
+            // Enter Password
+            verifyEquals(monitoringPlansPage.passwordLabel, "Password");
+            monitoringPlansPage.passwordField.sendKeys(password);
+            // Click Log In
+            verifyEquals(monitoringPlansPage.logInButtonModal, "Log In");
+            click(monitoringPlansPage.logInButtonModal);
+            // Welcome note appears on Dashboard Navigation
             // A Person Icon with the words "Welcome, [User's first name]"
-        verifyEquals(monitoringPlansPage.welcomeMessage, "Welcome, \"Moses\"");
+            waitFor(monitoringPlansPage.welcomeMessage);
+            verifyEquals(monitoringPlansPage.welcomeMessage, "Welcome, Moses");
             // The Log In button now says Log Out
-        //Click Log Out button
-        verifyEquals(monitoringPlansPage.logOutButton, "Log Out");
-        click(monitoringPlansPage.logOutButton);
-        // Confirm that the Logout has occurred
-        waitFor(monitoringPlansPage.logInButton);
-        verifyEquals(monitoringPlansPage.logInButton, "Log In");
+            //Click Log Out button
+            verifyEquals(monitoringPlansPage.logOutButton, "Log Out");
+            click(monitoringPlansPage.logOutButton);
+            // Confirm that the Logout has occurred
+            waitFor(monitoringPlansPage.logInButton);
+            verifyEquals(monitoringPlansPage.logInButton, "Log In");
 
+        }
     }
 }
