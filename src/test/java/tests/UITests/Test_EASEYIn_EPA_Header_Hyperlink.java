f7b27f9071b1ee4194a98d2c472e5675ec0e07ca
mosesdee <CVP4mode!>
2021-07-26 14:59:35 -0400

    New test for EPA_Header_Hyperlink

---
+++ src/test/java/tests/UITests/Test_EASEYIn_EPA_Header_Hyperlink.java
@@ -0,0 +1,28 @@
+package tests.UITests;
+
+import org.testng.annotations.Test;
+import pages.MonitoringPlansPage;
+import tests.utils.UITestBase;
+
+public class Test_EASEYIn_EPA_Header_Hyperlink extends UITestBase {
+
+    @Test
+    public void test() {
+
+//        Navigate to EASEY In
+//        https://easey-dev.app.cloud.gov/ecmps/monitoring-plans
+        goTo("https://easey-dev.app.cloud.gov/ecmps/monitoring-plans");
+
+        MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+
+        verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+
+        monitoringPlansPage.epaHeaderLogoAndHyperlink.isDisplayed();
+
+        click(monitoringPlansPage.epaHeaderLogoAndHyperlink);
+
+        changeTab();
+        verifyEquals(driver.getCurrentUrl(), "https://www.epa.gov/");
+
+    }
+}
