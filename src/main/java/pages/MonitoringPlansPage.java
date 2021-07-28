ebe329fb25f55ee82ffe9af9c1e14395362e55fa
mosesdee <CVP4mode!>
2021-07-28 08:41:33 -0400

    Fixed locator

--- src/main/java/pages/MonitoringPlansPage.java
+++ src/main/java/pages/MonitoringPlansPage.java
@@ -352,7 +352,7 @@ public class MonitoringPlansPage extends PageBase {
     @FindBy (css = "button[epa-testid='btnOpen']")
     public List<WebElement> configOpenButton;
 
-    @FindBy (css ="div[aria-label='open 3 (1, 2, CS0AAN)  tab")
+    @FindBy (css ="div[aria-label='open 3 (1, 2, CS0AAN)  tab']")
     public WebElement configTabBarry12CS0AAN;
 
 }
