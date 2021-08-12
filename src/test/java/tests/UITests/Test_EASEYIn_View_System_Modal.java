98567d9b32d79a6b8cde2082cb3c19195b323fa3
mosesdee <CVP4mode!>
2021-08-12 09:43:57 -0400

    New Tests Added

---
+++ src/test/java/tests/UITests/Test_EASEYIn_View_System_Modal.java
@@ -0,0 +1,63 @@
+package tests.UITests;
+
+import org.testng.annotations.Test;
+import pages.MonitoringPlansPage;
+import tests.utils.UITestBase;
+
+public class Test_EASEYIn_View_System_Modal extends UITestBase {
+
+    @Test
+    public void test() {
+
+//        Navigate to EASEY In
+//        https://easey-dev.app.cloud.gov/ecmps/monitoring-plans
+        goTo("https://easey-dev.app.cloud.gov/ecmps/monitoring-plans");
+
+        MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+
+        verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+
+        monitoringPlansPage.filterByKeywordBox.sendKeys("Barry");
+        click(monitoringPlansPage.filterByKeywordButton);
+
+        click(monitoringPlansPage.facilityCaretBarry);
+
+        waitFor(driver -> monitoringPlansPage.configOpenButton.size() > 1);
+        verifyEquals(monitoringPlansPage.configOpenButton.get(1), "Open");
+        click(monitoringPlansPage.configOpenButton.get(0));
+
+        click(monitoringPlansPage.configTabBarry12CS0AAN);
+
+        verifyEquals(monitoringPlansPage.accordionButtonMonPlan, "Methods");
+
+        click(monitoringPlansPage.monitoringSystems);
+
+        waitFor(monitoringPlansPage.accordionButtonMonPlan);
+        verifyEquals(monitoringPlansPage.accordionButtonMonPlan, "Systems");
+
+        verifyEquals(monitoringPlansPage.viewButton.get(0).getText(), "View");
+        click(monitoringPlansPage.viewButton.get(0));
+
+        verifyEquals(monitoringPlansPage.monPlanModalHeaderLabel, "System: AF1");
+
+        waitFor(monitoringPlansPage.systemFuelFlowsHeader);
+
+        verifyEquals(monitoringPlansPage.systemComponentsHeader, "System Components");
+
+        verifyEquals(monitoringPlansPage.systemFuelFlowsHeader, "Fuel Flows");
+
+        verifyEquals(monitoringPlansPage.closeModal, "Close");
+        click(monitoringPlansPage.closeModal);
+
+        verifyFalse(isDisplayed(monitoringPlansPage.monPlanModalHeaderLabel));
+
+        click(monitoringPlansPage.viewButton.get(0));
+
+        verifyEquals(monitoringPlansPage.monPlanModalHeaderLabel, "System: AF1");
+
+        click(monitoringPlansPage.xOutModal);
+
+        verifyFalse(isDisplayed(monitoringPlansPage.monPlanModalHeaderLabel));
+
+    }
+}
