12491101d191e28f4aff269af5a6d08fbab9d6be
mosesdee <CVP4mode!>
2021-09-30 14:09:18 -0400

    Reorganizing and Renaming the tests

---
+++ src/test/java/tests/UITests/EASEYIn_Emissioners/generalTests/Test_EASEYIn_MonPlan_Accordion.java
@@ -0,0 +1,42 @@
+package tests.UITests.EASEYIn_Emissioners.generalTests;
+
+import org.testng.annotations.Test;
+import pages.MonitoringPlansPage;
+import tests.utils.UITestBase;
+
+public class Test_EASEYIn_MonPlan_Accordion extends UITestBase {
+
+    @Test
+    public void test() {
+
+//        Navigate to EASEY In
+//        https://easey-dev.app.cloud.gov/ecmps/monitoring-plans
+        goTo("https://easey-dev.app.cloud.gov/ecmps/monitoring-plans");
+
+        MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+
+        verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+
+        input(monitoringPlansPage.filterByKeywordBox,"Barry");
+        click(monitoringPlansPage.filterByKeywordButton);
+
+        click(monitoringPlansPage.facilityCaretBarry);
+
+        waitFor(driver -> monitoringPlansPage.configOpenButton.size() > 1);
+        verifyEquals(monitoringPlansPage.configOpenButton.get(1), "Open");
+        click(monitoringPlansPage.configOpenButton.get(0));
+
+        click(monitoringPlansPage.configTabBarry12CS0AAN);
+
+        verifyEquals(monitoringPlansPage.accordionMethodsLabel, "Methods");
+
+        waitFor(monitoringPlansPage.monPlanActionsHeader);
+        verifyEquals(monitoringPlansPage.monPlanActionsHeader, "Actions");
+        verifyTrue(isDisplayed(monitoringPlansPage.monPlanActionsHeader));
+
+        click(monitoringPlansPage.accordionButtonMonPlan);
+
+        verifyFalse(isDisplayed(monitoringPlansPage.monPlanActionsHeader));
+
+    }
+}
---
+++ src/test/java/tests/UITests/EASEYIn_Emissioners/generalTests/Test_EASEYIn_Show_Inactive_Disabled.java
@@ -0,0 +1,42 @@
+package tests.UITests.EASEYIn_Emissioners.generalTests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import pages.MonitoringPlansPage;
+import tests.utils.UITestBase;
+
+public class Test_EASEYIn_Show_Inactive_Disabled extends UITestBase {
+
+    @Test
+    public void test() {
+
+//        Navigate to EASEY In
+//        https://easey-dev.app.cloud.gov/ecmps/monitoring-plans
+        goTo("https://easey-dev.app.cloud.gov/ecmps/monitoring-plans");
+
+        MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+
+        verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+
+        input(monitoringPlansPage.filterByKeywordBox,"Barry");
+        click(monitoringPlansPage.filterByKeywordButton);
+
+        click(monitoringPlansPage.facilityCaretBarry);
+
+        waitFor(driver -> monitoringPlansPage.configOpenButton.size() > 1);
+        verifyEquals(monitoringPlansPage.configOpenButton.get(1), "Open");
+        click(monitoringPlansPage.configOpenButton.get(0));
+
+        click(monitoringPlansPage.configTabBarry12CS0AAN);
+
+        verifyEquals(monitoringPlansPage.accordionMethodsLabel, "Methods");
+
+        click(monitoringPlansPage.monitoringSystems);
+
+        waitFor(monitoringPlansPage.accordionSystemsLabel);
+        verifyEquals(monitoringPlansPage.accordionSystemsLabel, "Systems");
+
+        verifyFalse(monitoringPlansPage.showInactiveCheckbox.findElement(By.id("checkbox")).isEnabled());
+
+    }
+}
