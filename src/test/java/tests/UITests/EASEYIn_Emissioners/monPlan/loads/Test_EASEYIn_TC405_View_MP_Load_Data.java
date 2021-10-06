5a8ec6f50b81eecc41704d08b963f1f6c3a89a13
mosesdee <CVP4mode!>
2021-10-06 14:54:57 -0400

    New Test: Test_EASEYIn_TC405_View_MP_Load_Data

---
+++ src/test/java/tests/UITests/EASEYIn_Emissioners/monPlan/loads/Test_EASEYIn_TC405_View_MP_Load_Data.java
@@ -0,0 +1,49 @@
+package tests.UITests.EASEYIn_Emissioners.monPlan.loads;
+
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import pages.MonitoringPlansPage;
+import tests.utils.UITestBase;
+
+public class Test_EASEYIn_TC405_View_MP_Load_Data extends UITestBase {
+
+    @Test
+    public void test() {
+
+//        Navigate to EASEY In
+//        https://easey-dev.app.cloud.gov/ecmps/monitoring-plans
+        goTo("https://easey-dev.app.cloud.gov/ecmps/monitoring-plans");
+
+        MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+        Actions action = new Actions(driver);
+
+        verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+
+        input(monitoringPlansPage.filterByKeywordBox, "Barry");
+        click(monitoringPlansPage.filterByKeywordButton);
+
+        click(monitoringPlansPage.facilityCaretBarry);
+
+        waitFor(driver -> monitoringPlansPage.configOpenButton.size() > 1);
+        verifyEquals(monitoringPlansPage.configOpenButton.get(1), "Open");
+        click(monitoringPlansPage.configOpenButton.get(0));
+
+        click(monitoringPlansPage.configTabBarry12CS0AAN);
+
+        verifyEquals(monitoringPlansPage.accordionMethodsLabel, "Methods");
+
+        click(monitoringPlansPage.monitoringLoads);
+
+        waitFor(monitoringPlansPage.monitoringLoads);
+        verifyEquals(monitoringPlansPage.monitoringLoads, "Loads");
+
+        click(monitoringPlansPage.location.get(2));
+        verifyEquals(monitoringPlansPage.location.get(2), "CS0AAN");
+
+        waitFor(driver -> monitoringPlansPage.viewButton.size() > 0);
+        action.moveToElement(monitoringPlansPage.viewButton.get(0)).click().build().perform();
+
+        verifyEquals(monitoringPlansPage.monPlanModalHeaderLabel, "Load");
+
+    }
+}
