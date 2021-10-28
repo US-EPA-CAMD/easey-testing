9ddcb385cfe422dd0e5e8a4d4fc4de92f786de4a
mosesdee <CVP4mode!>
2021-10-28 10:19:10 -0400

    Working on new test: Test_EASEYIn_TC1903_Prompt_for_Unsaved_Changes

---
+++ src/test/java/tests/UITests/EASEYIn_Emissioners/monPlan/methods/Test_EASEYIn_TC1903_Prompt_for_Unsaved_Changes.java
@@ -0,0 +1,41 @@
+package tests.UITests.EASEYIn_Emissioners.monPlan.methods;
+
+import org.testng.annotations.Test;
+import pages.MonitoringPlansPage;
+import tests.utils.UITestBase;
+
+public class Test_EASEYIn_TC1903_Prompt_for_Unsaved_Changes extends UITestBase {
+
+    @Test()
+    public void tests() {
+        String username = System.getenv("MOSES_TESTING_USERNAME");
+        String password = System.getenv("MOSES_TESTING_PASSWORD");
+
+        //Navigate to EASEY In
+        //https://easey-dev.app.cloud.gov/ecmps/monitoring-plans
+
+        goTo("https://easey-dev.app.cloud.gov/ecmps/monitoring-plans");
+
+        MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+
+        waitFor(monitoringPlansPage.title);
+        verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+
+        verifyEquals(monitoringPlansPage.logInButtonOpenModal, "Log In");
+        click(monitoringPlansPage.logInButtonOpenModal);
+
+        verifyEquals(monitoringPlansPage.usernameLabelModal.getText(), "Username");
+        input(monitoringPlansPage.usernameFieldModal, username);
+
+        verifyEquals(monitoringPlansPage.passwordLabelModal.getText(), "Password");
+        input(monitoringPlansPage.passwordFieldModal, password);
+
+        verifyEquals(monitoringPlansPage.logInButtonSubmit, "Log In");
+        click(monitoringPlansPage.logInButtonSubmit);
+
+        waitFor(monitoringPlansPage.title);
+        waitFor(monitoringPlansPage.dashWorkspace);
+        verifyEquals(monitoringPlansPage.dashWorkspace, "Workspace");
+
+    }
+}
