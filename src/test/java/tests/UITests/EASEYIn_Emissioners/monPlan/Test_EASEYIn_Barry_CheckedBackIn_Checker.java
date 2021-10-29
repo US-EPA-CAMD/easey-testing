c0817ab11a54163b88a6eb8135331a4298e41b6c
mosesdee <CVP4mode!>
2021-10-29 11:55:39 -0400

    New test: Test_EASEYIn_Barry_CheckedBackIn_Checker

---
+++ src/test/java/tests/UITests/EASEYIn_Emissioners/monPlan/Test_EASEYIn_Barry_CheckedBackIn_Checker.java
@@ -0,0 +1,67 @@
+package tests.UITests.EASEYIn_Emissioners.monPlan;
+
+import org.testng.annotations.Test;
+import pages.MonitoringPlansPage;
+import tests.utils.UITestBase;
+
+public class Test_EASEYIn_Barry_CheckedBackIn_Checker extends UITestBase {
+// This test is to make sure that Barry 1, 2, CS0AAN is Checked back in.
+    @Test()
+    public void tests() {
+        String username = System.getenv("MOSES_TESTING_USERNAME");
+        String password = System.getenv("MOSES_TESTING_PASSWORD");
+
+        //Navigate to EASEY In
+        //https://easey-dev.app.cloud.gov/ecmps/monitoring-plans
+
+        goTo("https://easey-dev.app.cloud.gov/ecmps/monitoring-plans");
+
+        MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+
+        waitFor(monitoringPlansPage.title);
+        verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+
+        verifyEquals(monitoringPlansPage.logInButtonOpenModal, "Log In");
+        click(monitoringPlansPage.logInButtonOpenModal);
+
+        verifyEquals(monitoringPlansPage.usernameLabelModal.getText(), "Username");
+        input(monitoringPlansPage.usernameFieldModal, username);
+
+        verifyEquals(monitoringPlansPage.passwordLabelModal.getText(), "Password");
+        input(monitoringPlansPage.passwordFieldModal, password);
+
+        verifyEquals(monitoringPlansPage.logInButtonSubmit, "Log In");
+        click(monitoringPlansPage.logInButtonSubmit);
+
+        waitFor(monitoringPlansPage.dashWorkspace);
+        verifyEquals(monitoringPlansPage.dashWorkspace, "Workspace");
+
+        verifyEquals(monitoringPlansPage.workspaceMonPlan, "Monitoring Plans");
+        click(monitoringPlansPage.workspaceMonPlan);
+
+        verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
+
+        waitFor(monitoringPlansPage.filterByKeywordBox);
+        input(monitoringPlansPage.filterByKeywordBox, "Barry");
+        click(monitoringPlansPage.filterByKeywordButton);
+
+        // Clicks on Barry (Oris Code 3)
+        click(monitoringPlansPage.facilityCaret.get(0));
+
+        waitFor(driver -> monitoringPlansPage.configOpenButton.size() > 1);
+        verifyEquals(monitoringPlansPage.configOpenButton.get(1), "Open");
+        click(monitoringPlansPage.configOpenButton.get(0));
+
+        click(monitoringPlansPage.configTabBarry12CS0AAN);
+
+        verifyEquals(monitoringPlansPage.accordionMethodsLabel, "Methods");
+
+        if (isDisplayed(monitoringPlansPage.configcheckInButton)) {
+            click(monitoringPlansPage.configcheckInButton);
+            verifyFalse(isDisplayed(monitoringPlansPage.configcheckInButton));
+        } else {
+            verifyTrue(isDisplayed(monitoringPlansPage.configcheckOutButton));
+        }
+
+    }
+}
