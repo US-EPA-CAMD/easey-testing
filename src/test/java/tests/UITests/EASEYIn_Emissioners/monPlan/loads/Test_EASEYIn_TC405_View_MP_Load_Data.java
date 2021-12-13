14e4847fcd3a8d6fe3ee9caf7a942bc27f067a76
mosesdee <CVP4mode!>
2021-12-13 12:56:53 -0500

    Fixing and improving tests

--- src/test/java/tests/UITests/EASEYIn_Emissioners/monPlan/loads/Test_EASEYIn_TC405_View_MP_Load_Data.java
+++ src/test/java/tests/UITests/EASEYIn_Emissioners/monPlan/loads/Test_EASEYIn_TC405_View_MP_Load_Data.java
@@ -1,5 +1,6 @@
 package tests.UITests.EASEYIn_Emissioners.monPlan.loads;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import pages.MonitoringPlansPage;
@@ -16,24 +17,39 @@ public class Test_EASEYIn_TC405_View_MP_Load_Data extends UITestBase {
 
         MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
         Actions action = new Actions(driver);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
+        // Verifying that the page has opened and the title is visible
+        waitFor(monitoringPlansPage.title);
         verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
 
+        // Selenium does not see the caret icon.
+        // This scrolls down so that the caret is visible.
+        js.executeScript("arguments[0].scrollIntoView(true);",
+                monitoringPlansPage.title);
+
+        // Using input box to search for desired facility
         waitFor(monitoringPlansPage.filterByKeywordBox);
-        input(monitoringPlansPage.filterByKeywordBox, "Barry");
+        input(monitoringPlansPage.filterByKeywordBox,"Barry");
         click(monitoringPlansPage.filterByKeywordButton);
 
         // Clicks on Barry (Oris Code 3)
         click(monitoringPlansPage.facilityCaret.get(0));
 
+        // Opens the desired configuration
         waitFor(driver -> monitoringPlansPage.configOpenButton.size() > 1);
-        verifyEquals(monitoringPlansPage.configOpenButton.get(1), "Open");
+        verifyEquals(monitoringPlansPage.configOpenButton.get(0), "Open");
         click(monitoringPlansPage.configOpenButton.get(0));
 
         click(monitoringPlansPage.configTabBarry12CS0AAN);
 
+        js.executeScript("window.scrollBy(0,250)", "");
+        js.executeScript("document.body.style.zoom = '0.6'");
+
+        waitFor(monitoringPlansPage.accordionMethodsLabel);
         verifyEquals(monitoringPlansPage.accordionMethodsLabel, "Methods");
 
+        waitFor(monitoringPlansPage.monitoringLoads);
         click(monitoringPlansPage.monitoringLoads);
 
         waitFor(monitoringPlansPage.accordionLoadsLabel);
@@ -45,6 +61,7 @@ public class Test_EASEYIn_TC405_View_MP_Load_Data extends UITestBase {
         waitFor(driver -> monitoringPlansPage.viewButton.size() > 0);
         action.moveToElement(monitoringPlansPage.viewButton.get(0)).click().build().perform();
 
+        waitFor(monitoringPlansPage.monPlanModalHeaderLabel);
         verifyEquals(monitoringPlansPage.monPlanModalHeaderLabel, "Load");
 
     }
