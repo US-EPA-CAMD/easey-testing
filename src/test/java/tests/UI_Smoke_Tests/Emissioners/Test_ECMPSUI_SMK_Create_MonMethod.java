10d011a095efe153f3325f025ee322656059802a
mosesdee <CVP4mode!>
2022-08-23 15:30:53 -0400

    Fixing Test: Test_ECMPSUI_SMK_Create_MonMethod

--- src/test/java/tests/UI_Smoke_Tests/Emissioners/Test_ECMPSUI_SMK_Create_MonMethod.java
+++ src/test/java/tests/UI_Smoke_Tests/Emissioners/Test_ECMPSUI_SMK_Create_MonMethod.java
@@ -106,11 +106,14 @@ public class Test_ECMPSUI_SMK_Create_MonMethod extends UITestBase {
         verifyEquals(monitoringPlansPage.revertModalYesButton, "Yes");
         click(monitoringPlansPage.revertModalYesButton);
         waitFor(driver -> !isDisplayed(monitoringPlansPage.revertModalYesButton));
+        waitFor(driver -> !isDisplayed(monitoringPlansPage.stopAnimationButton));
 
-        // These steps closes the tab and automatically Checks Back In the configuration
-        click(monitoringPlansPage.closeConfigTab.get(0));
-        waitFor(monitoringPlansPage.selectConfigurationsLabel);
-        verifyTrue(isDisplayed(monitoringPlansPage.selectConfigurationsLabel));
+        // These steps Checks Back In the configuration
+        waitFor(monitoringPlansPage.configcheckBackInButton);
+        click(monitoringPlansPage.configcheckBackInButton);
+
+        waitFor(monitoringPlansPage.configcheckOutButton);
+        verifyEquals(monitoringPlansPage.configcheckOutButton, "Check Out");
 
     }
     @Override
