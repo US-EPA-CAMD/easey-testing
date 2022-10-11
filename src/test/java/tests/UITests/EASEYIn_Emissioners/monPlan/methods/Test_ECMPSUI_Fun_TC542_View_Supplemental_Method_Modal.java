d3e3a6b6a59f846c7422f4a9e66b9c34027eac59
mosesdee <CVP4mode!>
2022-10-11 15:15:22 -0400

    Updates

--- src/test/java/tests/UITests/EASEYIn_Emissioners/monPlan/methods/Test_ECMPSUI_Fun_TC542_View_Supplemental_Method_Modal.java
+++ src/test/java/tests/UITests/EASEYIn_Emissioners/monPlan/methods/Test_ECMPSUI_Fun_TC542_View_Supplemental_Method_Modal.java
@@ -1,5 +1,6 @@
 package tests.UITests.EASEYIn_Emissioners.monPlan.methods;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 import pages.MonitoringPlansPage;
 import tests.utils.UITestBase;
@@ -14,6 +15,7 @@ public class Test_ECMPSUI_Fun_TC542_View_Supplemental_Method_Modal extends UITes
         goToo("ecmps","/monitoring-plans");
 
         MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
         verifyEquals(monitoringPlansPage.title, "Monitoring Plans");
 
@@ -29,29 +31,17 @@ public class Test_ECMPSUI_Fun_TC542_View_Supplemental_Method_Modal extends UITes
 
         click(monitoringPlansPage.configTabs.get(0));
 
+
         waitFor(monitoringPlansPage.accordionSupplementalMethodsLabel);
         verifyEquals(monitoringPlansPage.accordionSupplementalMethodsLabel, "Supplemental Methods");
-
         waitFor(driver -> monitoringPlansPage.viewButton.size() > 8);
+        js.executeScript("arguments[0].scrollIntoView(true);", monitoringPlansPage.viewButton.get(8));
+
         verifyEquals(monitoringPlansPage.viewButton.get(8).getText(), "View");
         click(monitoringPlansPage.viewButton.get(8));
 
         waitFor(monitoringPlansPage.monPlanModalHeaderLabel);
         verifyEquals(monitoringPlansPage.monPlanModalHeaderLabel, "Component: Monitoring MATS Methods");
-
-        verifyEquals(monitoringPlansPage.closeModal, "Close");
-        click(monitoringPlansPage.closeModal);
-
-        verifyFalse(isDisplayed(monitoringPlansPage.monPlanModalHeaderLabel));
-
-        click(monitoringPlansPage.viewButton.get(8));
-
-        verifyEquals(monitoringPlansPage.monPlanModalHeaderLabel, "Component: Monitoring MATS Methods");
-
-        click(monitoringPlansPage.xOutModal);
-
-        waitFor(monitoringPlansPage.accordionSupplementalMethodsLabel);
-        verifyFalse(isDisplayed(monitoringPlansPage.monPlanModalHeaderLabel));
 
     }
 }
