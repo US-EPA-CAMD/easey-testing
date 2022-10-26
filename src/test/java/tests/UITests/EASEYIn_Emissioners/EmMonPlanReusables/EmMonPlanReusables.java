8c74fac8239bde63302b5ef1fc39d2d8d6b7d692
mosesdee <CVP4mode!>
2022-10-26 14:19:07 -0400

    Improving/Fixing Tests

--- src/test/java/tests/UITests/EASEYIn_Emissioners/EmMonPlanReusables/EmMonPlanReusables.java
+++ src/test/java/tests/UITests/EASEYIn_Emissioners/EmMonPlanReusables/EmMonPlanReusables.java
@@ -6,6 +6,32 @@ import tests.utils.UITestBase;
 
 public class EmMonPlanReusables extends UITestBase {
 
+    protected void revertToOfficial() {
+        MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        js.executeScript("arguments[0].scrollIntoView(true);",
+                monitoringPlansPage.revertOfficialRecordButton);
+
+        waitFor(monitoringPlansPage.revertOfficialRecordButton);
+        verifyEquals(monitoringPlansPage.revertOfficialRecordButton, "Revert to Official Record");
+        click(monitoringPlansPage.revertOfficialRecordButton);
+
+        waitFor(monitoringPlansPage.revertModalYesButton);
+        verifyEquals(monitoringPlansPage.revertModalYesButton, "Yes");
+        click(monitoringPlansPage.revertModalYesButton);
+        waitFor(driver -> !isDisplayed(monitoringPlansPage.revertModalYesButton));
+        waitFor(driver -> !isDisplayed(monitoringPlansPage.stopAnimationButton));
+        waitFor(driver -> !isDisplayed(monitoringPlansPage.stopAnimationImage));
+
+        // These steps Checks Back In the configuration
+        waitFor(monitoringPlansPage.configcheckBackInButton);
+        click(monitoringPlansPage.configcheckBackInButton);
+
+        waitFor(monitoringPlansPage.configcheckOutButton);
+        verifyEquals(monitoringPlansPage.configcheckOutButton, "Check Out");
+    }
+
     protected void logOutMethod() {
 
         MonitoringPlansPage monitoringPlansPage = new MonitoringPlansPage(driver);
