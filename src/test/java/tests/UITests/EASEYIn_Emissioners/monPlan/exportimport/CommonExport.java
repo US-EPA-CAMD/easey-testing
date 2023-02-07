17e1e2b4c2600942dabb19bc514071552187d475
mosesdee <CVP4mode!>
2023-02-07 15:41:18 -0500

    Test Update

--- src/test/java/tests/UITests/EASEYIn_Emissioners/monPlan/exportimport/CommonExport.java
+++ src/test/java/tests/UITests/EASEYIn_Emissioners/monPlan/exportimport/CommonExport.java
@@ -2,8 +2,7 @@ package tests.UITests.EASEYIn_Emissioners.monPlan.exportimport;
 
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
-import tests.UITests.EASEYIn_Emissioners.EmMonPlanReusables.EmMonPlanReusables;
-import tests.utils.UITestBase;
+import tests.UITests.UiReusableComponents.UiReusableComponents;
 
 import java.io.File;
 import java.text.Format;
@@ -11,7 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
-public class CommonExport extends EmMonPlanReusables {
+public class CommonExport extends UiReusableComponents {
 
 
     public static File getLatestFileFromDir(String directoryFilePath) {
