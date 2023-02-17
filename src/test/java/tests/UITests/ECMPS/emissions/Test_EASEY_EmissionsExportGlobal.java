95766786a6d94b322ecce96bc6361a8aa1785fb1
mosesdee <CVP4mode!>
2023-02-17 11:16:42 -0500

    Updating folder locations and names

--- src/test/java/tests/UITests/ECMPS/emissions/Test_EASEY_EmissionsExportGlobal.java
+++
@@ -1,9 +0,0 @@
-package tests.UITests.ECMPS.emissions;
-
-import org.testng.annotations.Test;
-import pages.EmissionsPage;
-import pages.MonitoringPlansPage;
-import tests.UITests.ECMPS.exportImport.CommonExport;
-
-public class Test_EASEY_EmissionsExportGlobal extends CommonExport {
-}
