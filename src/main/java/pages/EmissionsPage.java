eef475763560a8f9b0b097724d0bc3e095c1c098
mackenzieharwood <mackenzieharwood@cvpcorp.com>
2023-02-27 15:27:45 -0500

    add emmisions page props# Please enter the commit message for your changes. Lines starting

--- src/main/java/pages/EmissionsPage.java
+++ src/main/java/pages/EmissionsPage.java
@@ -1,9 +1,32 @@
 package pages;
 
         import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.support.FindBy;
 
-public class EmissionsPage extends HeaderFooterAndHomePage {
+public class EmissionsPage extends MonitoringPlansPage {
 
     public EmissionsPage(WebDriver driver) { super(driver); }
+
+    @FindBy(xpath = "//h2[contains(text(),'Emissions')]")
+    public WebElement title;
+
+    @FindBy(xpath = "//button[contains(text(),'Apply Filter(s)')]")
+    public WebElement applyFilter;
+
+    @FindBy(xpath = "//button[contains(text(),'Export Data')]")
+    public WebElement exportData;
+
+    @FindBy ( id="viewtemplate")
+    public WebElement viewTemplateBox;
+    //VIEW TEMPLATE OPTIONS
+    @FindBy (xpath = "//option[contains(text(),'Hourly SO2 CEMS View')]")
+    public WebElement hourlyS02cemsViewOption;
+
+    @FindBy (xpath = "//option[contains(text(),'Hourly Combined Parameters View')]")
+    public WebElement hourlyCombinedParametersViewOption;
+
+    @FindBy (xpath = "//option[contains(text(),'Hourly Heat Input CEMS View')]")
+    public WebElement hourlyHeatInputcemsViewOption;
 
 }
