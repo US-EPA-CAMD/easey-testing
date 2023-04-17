aa4140c404c5f109e7033346471dcc4f9e3c1603
mackenzieharwood <mackenzieharwood@cvpcorp.com>
2023-04-17 14:53:22 -0400

    clean up comments

--- src/test/java/tests/UITests/ECMPS/exportImport/Test_EASEY_MonPlanExportALLGlobal.java
+++ src/test/java/tests/UITests/ECMPS/exportImport/Test_EASEY_MonPlanExportALLGlobal.java
@@ -27,14 +27,12 @@ public class Test_EASEY_MonPlanExportALLGlobal extends CommonExport {
         //Search for facility
         input(monitoringPlansPage.filterByKeywordBox,"Smith Generating");
         click(monitoringPlansPage.filterByKeywordButton);
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        js.executeScript("window.scrollBy(0,350)", "");
+
 
         // Clicks on Smith
         click(monitoringPlansPage.facilityCaret.get(0));
 
         waitFor(driver -> monitoringPlansPage.configOpenButton.size() > 1);
-//        verifyEquals(monitoringPlansPage.configOpenButton.get(0), "Open");
 
 
         click(monitoringPlansPage.configOpenButton.get(0));
