9051caf3fc7ebfc975e25ea357c28a6c08dc03fa
mackenzieharwood <mackenzieharwood@cvpcorp.com>
2023-04-24 14:05:13 -0400

    add upload.json for emissions import

--- src/test/java/tests/UITests/ECMPS/emissions/Test_EASEY_EmissionsImport.java
+++ src/test/java/tests/UITests/ECMPS/emissions/Test_EASEY_EmissionsImport.java
@@ -16,61 +16,55 @@ public class Test_EASEY_EmissionsImport  extends CommonExport {
 
     @Test
     public void test() throws InterruptedException {
-        // Navigate to EASEY In
-        //https://easey-dev.app.cloud.gov/ecmps/monitoring-plans
-        goToo("ecmps","/monitoring-plans");
-        EmissionsPage emissionsPage = new EmissionsPage(driver);
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+        //        Navigate to EASEY In
+        //        https://easey-dev.app.cloud.gov/ecmps/monitoring-plans
+        goToo("ecmps","/emissions");
 
-        //wait for page to load, verify page is MP
-        waitFor(emissionsPage.title);
+        EmissionsPage emissionsPage = new EmissionsPage(driver);
+
         verifyEquals(emissionsPage.title, "Emissions");
 
-        //open log in modal
         verifyEquals(emissionsPage.logInButtonOpenModal, "Log In");
         click(emissionsPage.logInButtonOpenModal);
-        //give username
+
         verifyEquals(emissionsPage.usernameLabelModal.getText(), "Username");
         input(emissionsPage.usernameFieldModal, username);
-        ////give password
+
         verifyEquals(emissionsPage.passwordLabelModal.getText(), "Password");
         input(emissionsPage.passwordFieldModal, password);
-        //verify log in btn is visible and click log in
+
         verifyEquals(emissionsPage.logInButtonSubmit, "Log In");
         click(emissionsPage.logInButtonSubmit);
-        //wait for log in to complete
-        waitFor(emissionsPage.dashWorkspace);
-        verifyEquals(emissionsPage.dashWorkspace, "Workspace");
-        //verify logged in and page is Emissions
-        verifyEquals(emissionsPage.workspaceMonPlan, "Monitoring Plans");
-        click(emissionsPage.workspaceMonPlan);
-        //verify the page is MP
-        verifyEquals(emissionsPage.title, "Monitoring Plans");
-        //wait for search bar to be visible
+
+        waitFor(driver -> !isDisplayed(emissionsPage.logInButtonSubmit));
+
+
+
+        waitFor(emissionsPage.title);
+
+        verifyEquals(emissionsPage.title, "Emissions");
+
+
         waitFor(emissionsPage.filterByKeywordBox);
 
         //Search for facility
         input(emissionsPage.filterByKeywordBox,"Smith Generating");
         click(emissionsPage.filterByKeywordButton);
 
-        // Clicks on first search result
+        // Clicks on Smith
         click(emissionsPage.facilityCaret.get(0));
 
-        //waits for return
         waitFor(driver -> emissionsPage.configOpenButton.size() > 1);
 
-        //verifies at least one search result returns
-        verifyEquals(emissionsPage.configOpenButton.get(1), "Open");
-        //clicks "open" button for first result
-        js.executeScript("arguments[0].scrollIntoView(true);",
-                emissionsPage.configOpenButton.get(5));
-        click(emissionsPage.configOpenButton.get(5));
-        Thread.sleep(3000);
+        click(emissionsPage.configOpenButton.get(0));
+
+
+        //wait for the tab to load
+        waitFor(emissionsPage.configTab1);
 
         // Clicks on Smith Tab
-        //configTabSmith
-        waitFor(emissionsPage.configTabSmith);
-        click(emissionsPage.configTabSmith);
+        click(emissionsPage.configTab1);
+
         //waits for checkout btn
         waitFor(emissionsPage.checkOutBTN);
         //click checkout button
@@ -80,15 +74,24 @@ public class Test_EASEY_EmissionsImport  extends CommonExport {
         waitFor(emissionsPage.importButton);
         //clicks import button
         click(emissionsPage.importButton);
-        Thread.sleep(1000);
 
-        //click(monitoringPlansPage.uploadFileChoiceButton);
+        waitFor(emissionsPage.importType);
+
+        click(emissionsPage.importType);
+
+        waitFor(emissionsPage.importFromFileOption);
+
+        click(emissionsPage.importFromFileOption);
+
+        ////input[@id='file-input-single'] is the hyperlinked text in "Drag file here or choose from folder" text
         WebElement upload_file = driver.findElement(By.xpath("//input[@id='file-input-single']"));
-        upload_file.sendKeys("C:\\Users\\mosesdee\\IdeaProjects\\easey-testing\\src\\test\\java\\tests\\UITests\\ECMPS\\exportImport\\files\\upload.json");
+        upload_file.sendKeys("C:\\EPA\\easey-testing\\src\\test\\java\\tests\\UITests\\ECMPS\\emissions\\files\\upload.json");
+
         //wait for import button to show
         waitFor(emissionsPage.importSubmitBTN);
         //click import button
         click(emissionsPage.importSubmitBTN);
+
         //wait for success
         waitFor(emissionsPage.successMessage);
 
