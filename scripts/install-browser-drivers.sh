1bd098f9856fec69ece39e1e878d3864f3f8de1f
mackenzieharwood <mackenzieharwood@cvpcorp.com>
2023-06-27 13:37:55 -0400

    add sudo

--- scripts/install-browser-drivers.sh
+++ scripts/install-browser-drivers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eo pipefail
-apt-get install -y wget
+sudo apt-get install -y wget
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install ./google-chrome-stable_current_amd64.deb
