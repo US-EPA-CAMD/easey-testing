3a724350f45f072da9dc4ee1d92942c46cfba159
mackenzieharwood <mackenzieharwood@cvpcorp.com>
2023-06-27 13:21:12 -0400

    remove cd command

--- scripts/install-browser-drivers.sh
+++ scripts/install-browser-drivers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -eo pipefail
-cd /usr/bin
 apt-get install -y wget
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install ./google-chrome-stable_current_amd64.deb
